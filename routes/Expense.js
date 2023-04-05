cc7da7d855e6337ea119f09abe63645dad17a3dc
aditya <adityamodhave2014@gmail.com>
2023-04-05 20:54:42 +0530

    first commit

---
+++ routes/Expense.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../Controllers/Expense");
+const expensesMiddle = require("../middleware/authMiddleware");
+
+router.get(
+  "/users",
+  expensesMiddle.authUser,
+  userController.getExpenses
+);
+
+router.post("/users", expensesMiddle.authUser, userController.postExpenses);
+
+router.delete("/users/:id", userController.deleteExpense);
+
+
+module.exports = router;
